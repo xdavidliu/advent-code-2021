827960eca44a0a074cee35acf5c582297b58c390
David Liu <xdavidliu@gmail.com>
2021-12-05 12:07:20 -0500

    Update day05.py

--- day05.py
+++ day05.py
@@ -1,27 +1,14 @@
-import math
-
-class Segment:
-    def _XY(pstr):
-        x, y = pstr.split(',')
-        return int(x), int(y)
-
-    def __init__(self, x1, y1, x2, y2):
-        self._x1, self._y1 = x1, y1
-        self._x2, self._y2 = x2, y2
-
-    def __repr__(self):
-        return f'Segment(x1={self._x1}, y1={self._y1}, x2={self._x2}, y2={self._y2})'
-
-
-def ReadSegment(line):
-    p, q = line.strip().split(' -> ')
-    return Segment(*Segment._XY(p), *Segment._XY(q))
-    
+from math import copysign
 
 def ReadAll(filename):
+    def Point(s):
+        x, y = s.split(',')
+        return int(x), int(y)
+    def ReadSegment(line):
+        p, q = line.strip().split(' -> ')
+        return Point(p) + Point(q)
     with open(filename) as f:
         return [ReadSegment(l) for l in f.readlines()]
-    
 
 class Grid:
     def __init__(self, size, include_diag):
@@ -29,16 +16,14 @@ class Grid:
         self._data = [[0] * size for _ in range(size)]
 
     def _Delta(x1, x2):
-        return 0 if x1 == x2 else int(math.copysign(1, x2 - x1))
+        return 0 if x1 == x2 else int(copysign(1, x2 - x1))
 
-    def Mark(self, seg):
-        x, y = seg._x1, seg._y1
-        dx = Grid._Delta(seg._x1, seg._x2)
-        dy = Grid._Delta(seg._y1, seg._y2)
+    def Mark(self, x, y, x2, y2):
+        dx, dy = Grid._Delta(x, x2), Grid._Delta(y, y2)
         if dx and dy and not self._include_diag:
             return
         self._data[x][y] += 1
-        while (x, y) != (seg._x2, seg._y2):
+        while (x, y) != (x2, y2):
             x += dx
             y += dy
             self._data[x][y] += 1
@@ -46,11 +31,9 @@ class Grid:
     def Score(self):
         return sum(1 for r in self._data for x in r if x > 1)
 
-
 if __name__ == '__main__':
     segs = ReadAll('data.txt')
-    include_diag = False
-    grid = Grid(1000, include_diag)
+    grid = Grid(1000, include_diag=True)
     for s in segs:
-        grid.Mark(s)
+        grid.Mark(*s)
     print(grid.Score())
