f600e521efeb40bfec4a5f7d660fda8937be7545
David Liu <xdavidliu@gmail.com>
2021-12-07 00:31:25 -0500

    Update day07.py

--- day07.py
+++ day07.py
@@ -1,11 +1,7 @@
-def score1(xs, a):
-    return sum(abs(x - a) for x in xs)
-
-def score2(xs, a):
-    def f(k):
-        return k*(k+1)//2
-    return sum(f(abs(x-a)) for x in xs)
+def f1(d): return d
+def f2(d): return d*(d+1)//2
+def score(xs, a, f): return sum(f(abs(x-a)) for x in xs)
 
 with open('data1.txt') as f:
     xs = [int(x) for x in f.read().strip().split(',')]
-print(min(score2(xs, a) for a in range(min(xs), max(xs) + 1)))
+print(min(score(xs, a, f2) for a in range(min(xs), max(xs) + 1)))
