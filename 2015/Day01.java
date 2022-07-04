dde0baadb0c65b20eb3981ff8185fd3fe1b0d916
David Liu <xdavidliu@gmail.com>
2022-07-04 17:42:29 -0400

    Update Day01.java

--- 2015/Day01.java
+++ 2015/Day01.java
@@ -1,1 +1,37 @@
-// foo bar baz
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Day01 {
+    private static int solve(String input) {
+        int score = 0, pos = 0;
+        boolean part2 = true;
+        for (char c : input.toCharArray()) {
+            switch(c) {
+                case '(':
+                    ++score;
+                    break;
+                case ')':
+                    --score;
+                    break;
+            }
+            ++pos;
+            if (part2 && score == -1) {
+                part2 = false;
+                System.out.printf("part 2 = %d\n", pos);
+            }
+        }
+        return score;
+    }
+    public static void main(String[] args) {
+        try {
+            String path = "c:\\users\\xdavi\\documents\\temp\\data01.txt";
+            BufferedReader read = new BufferedReader(new FileReader(path));
+            String ln = read.readLine();
+            System.out.printf("part 1 = %d\n", solve(ln));
+        } catch(FileNotFoundException e) {
+        } catch(IOException e) {
+        }
+    }
+}
