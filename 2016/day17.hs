5596cfcda4b29b8fde433f3638b8a8079f6696ed
David Liu <xdavidliu@gmail.com>
2022-08-15 21:39:40 -0400

    Update day17.hs

--- 2016/day17.hs
+++ 2016/day17.hs
@@ -6,13 +6,12 @@ import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
 
 
---input = "pgflpeqp"
-input = "ihgpwlah"
+input = "pgflpeqp"
 main = do
   putStr "part 1 = "
   putStrLn $ solvePart1 ""
   putStr "part 2 = "
-  -- print $ solvePart2 ""
+  print $ solvePart2 ""
 
 md5digest z = show (md5 $ pack z)
 open c = 10 < digitToInt c 
@@ -28,29 +27,29 @@ position s = rec s (0,0)
         rec ('R':xs) (a,b) = rec xs (a+1, b)
         rec _ _ = error "rec"
 
-solvePart1 s = bfs (Seq.singleton s) s
-solvePart2 s = length $ bfs (Seq.singleton s) s
+solvePart1 s = bfs (Seq.singleton s) s True
+solvePart2 s = length $ bfs (Seq.singleton s) s False
 
-bfs :: Seq String -> String -> String
-bfs q t = case Seq.viewl q of
+bfs :: Seq String -> String -> Bool -> String
+bfs q t short = case Seq.viewl q of
   Seq.EmptyL -> t
-  s Seq.:< qRest -> explore s qRest
+  s Seq.:< qRest -> explore s qRest short t
 
 goal = (3,3)
 
-explore s q = enqueue udlr s q
+explore s q short t = enqueue udlr s q short tt
   where (u:d:l:r:_) = md5digest $ input ++ s
-        (x,y) = position s
+        p@(x,y) = position s
+        isGoal = p == goal
+        tt = if isGoal then s else t
         uu = strIf "U" (y /= 0 && open u)
         dd = strIf "D" (y /= 3 && open d)
         ll = strIf "L" (x /= 0 && open l)
         rr = strIf "R" (x /= 3 && open r)
-        udlr = dd ++ rr ++ uu ++ ll
+        udlr = if isGoal then "" else dd ++ rr ++ uu ++ ll
 
-enqueue "" s q = bfs q s
-enqueue (x:xs) s q
-  | position t == goal = t
-  | otherwise = enqueue xs s (q Seq.|> t)
-  where t = s ++ [x]
-
--- TODO: gotta STOP when hit (3,3), so don't use target
+enqueue "" s q short t = bfs q t short
+enqueue (x:xs) s q short t
+  | short && position k == goal = k
+  | otherwise = enqueue xs s (q Seq.|> k) short t
+  where k = s ++ [x]
