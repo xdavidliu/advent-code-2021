64284fb387dd91a74c1ad3886c9260f88170b89b
David Liu <xdavidliu@gmail.com>
2022-08-18 08:34:13 -0400

    Update day12.hs

--- 2016/day12.hs
+++ 2016/day12.hs
@@ -15,45 +15,35 @@ main = do
     print $ value A ans2
     hClose han
 
-data Reg = A | B | C | D deriving (Show)
-data Ins = CpI Int Reg | CpR Reg Reg | Inc Int Reg |
-           JnzI Int Int | JnzR Reg Int deriving (Show)
+data Ins = Cpy Val Val | Inc Int Val | Jnz Val Val
+data Val = A | B | C | D | Val Int
 
 parse :: String -> Ins
-parse ln = case head ws of
-  "cpy" -> parseCpy $ tail ws
-  "inc" -> Inc 1 reg
-  "dec" -> Inc (-1) reg
-  "jnz" -> parseJnz $ tail ws
-  _ -> error "parse"
-  where ws = words ln
-        reg = parseReg (ws !! 1)
+parse ln = case cmd of
+  "cpy" -> Cpy val1 val2
+  "inc" -> Inc 1 val1
+  "dec" -> Inc (-1) val1
+  "jnz" -> Jnz val1 val2
+  _ -> undefined
+  where (cmd:args) = words ln
+        val1 = parseVal $ head args
+        val2 = parseVal $ last args
 
-parseCpy :: [String] -> Ins
-parseCpy [x, y] = case readMaybe x of
-  Just i -> CpI i reg2
-  Nothing -> CpR reg1 reg2
-  where reg1 = parseReg x
-        reg2 = parseReg y
-parseCpy _ = error "parseCpy"
+parseVal s = case readMaybe s of
+  Just i -> Val i
+  Nothing -> parseReg s
 
-parseJnz :: [String] -> Ins
-parseJnz [x, y] = case readMaybe x of
-  Just i -> JnzI i k
-  Nothing -> JnzR (parseReg x) k
-  where k = read y
-parseJnz _ = error "parseJnz"
-
-parseReg :: String -> Reg
+parseReg :: String -> Val
 parseReg "a" = A
 parseReg "b" = B
 parseReg "c" = C
 parseReg "d" = D
-parseReg _ = error "parseReg"
+parseReg _ = undefined
 
 type Regs = (Int, Int, Int, Int)
 
-value :: Reg -> Regs -> Int
+value :: Val -> Regs -> Int
+value (Val x) _ = x
 value A (v,_,_,_) = v
 value B (_,v,_,_) = v
 value C (_,_,v,_) = v
@@ -61,10 +51,8 @@ value D (_,_,_,v) = v
 
 perform :: [Ins] -> [Ins] -> Regs -> Regs
 perform _ [] regs = regs
-perform before rest@(JnzI i k : _) regs =
-  performJump i k before rest regs
-perform before rest@(JnzR r k : _) regs =
-  performJump (value r regs) k before rest regs
+perform before rest@(Jnz v k : _) regs =
+  performJump (value v regs) (value k regs) before rest regs
 perform before (ins : after) regs =
   perform (ins:before) after (performOne ins regs)
 
@@ -78,17 +66,18 @@ performJump i k before rest regs
         ins = head rest
         after = tail rest
 
-performOne (CpI i r) regs = storeVal i r regs
-performOne (CpR t r) regs = storeVal (value t regs) r regs
-performOne (Inc i r) regs = addVal i r regs
-performOne _ _ = error "performOne"
+performOne (Cpy p q) regs = storeVal (value p regs) q regs
+performOne (Inc i q) regs = addVal i q regs
+performOne _ _ = undefined
 
 storeVal i A (a,b,c,d) = (i,b,c,d)
 storeVal i B (a,b,c,d) = (a,i,c,d)
 storeVal i C (a,b,c,d) = (a,b,i,d)
 storeVal i D (a,b,c,d) = (a,b,c,i)
+storeVal _ (Val _) rs = rs
 
 addVal i A (a,b,c,d) = (a+i,b,c,d)
 addVal i B (a,b,c,d) = (a,b+i,c,d)
 addVal i C (a,b,c,d) = (a,b,c+i,d)
 addVal i D (a,b,c,d) = (a,b,c,d+i)
+addVal i (Val x) rs = rs
