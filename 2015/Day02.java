4b9e772972a1aa4f74bff6b8452e16c4014c5106
David Liu <xdavidliu@gmail.com>
2022-08-20 07:27:46 -0400

    Update Day02.java

--- 2015/Day02.java
+++ 2015/Day02.java
@@ -1,28 +1,32 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 public class Day02 {
+    public static void main(String[] args) throws IOException {
+        var dims = Files
+                .lines(Path.of("/home/xdavidliu/Documents/temp/data02.txt"))
+                .map(Day02::dimensions).toList();
+        System.out.format("part 1 = %d, part 2 = %d\n",
+                dims.stream().mapToInt(Day02::part1).sum(),
+                dims.stream().mapToInt(Day02::part2).sum());
+    }
     private static int[] dimensions(String ln) {
-        var mch = java.util.regex.Pattern.compile(
-                "(\\d+)x(\\d+)x(\\d+)").matcher(ln);
-        if (mch.matches()) {
-            return java.util.stream.Stream.of(1,2,3)
-                    .map(i -> Integer.decode(mch.group(i))).mapToInt(i -> i).toArray();
-        } else throw new RuntimeException("invalid data");
+        var mch = Pattern.compile("(\\d+)x(\\d+)x(\\d+)").matcher(ln);
+        if (!mch.matches()) throw new RuntimeException("invalid data: " + ln);
+        return Stream.of(1,2,3).mapToInt(i -> Integer.decode(mch.group(i))).toArray();
     }
     private static int part1(int[] dim) {
-        int ab = dim[0] * dim[1], bc = dim[1] * dim[2], ac = dim[0] * dim[2];
-        int min = Integer.min(ab, Integer.min(bc, ac));
-        return 2 * (ab + bc + ac) + min;
+        return Arrays.stream(dim).min().getAsInt() + 2 *
+                Stream.of(0,1,2).mapToInt(i -> dim[i] * dim[(i+1)%3]).sum();
     }
     private static int part2(int[] dim) {
-        int max = Integer.max(dim[0], Integer.max(dim[1], dim[2]));
-        int prod = dim[0] * dim[1] * dim[2];
-        return prod + 2 * (dim[0] + dim[1] + dim[2] - max);
-    }
-    public static void main(String[] args) throws java.io.IOException {
-        var lines = java.nio.file.Files.lines(
-                java.nio.file.Path.of("/Users/xdavidliu/Documents/temp/data02.txt"));
-        var dims = lines.map(Day02::dimensions).toList();
-        System.out.format("part 1 = %d, part 2 = %d\n",
-                dims.stream().map(Day02::part1).mapToInt(i -> i).sum(),
-                dims.stream().map(Day02::part2).mapToInt(i -> i).sum());
+        int max = Arrays.stream(dim).max().getAsInt();
+        int prod = Arrays.stream(dim).reduce((x, y) -> x * y).getAsInt();
+        int sum = Arrays.stream(dim).sum();
+        return prod + 2 * (sum - max);
     }
 }
