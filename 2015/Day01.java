8e94aeb1fd199f63a49cdd801a7a1a7fa1d58927
David Liu <xdavidliu@gmail.com>
2022-08-19 21:47:28 -0400

    Update Day01.java

--- 2015/Day01.java
+++ 2015/Day01.java
@@ -1,7 +1,6 @@
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Day01 {
     private static int solve(String input) {
@@ -9,12 +8,8 @@ public class Day01 {
         boolean part2 = true;
         for (char c : input.toCharArray()) {
             switch(c) {
-                case '(':
-                    ++score;
-                    break;
-                case ')':
-                    --score;
-                    break;
+                case '(' -> ++score;
+                case ')' -> --score;
             }
             ++pos;
             if (part2 && score == -1) {
@@ -24,14 +19,9 @@ public class Day01 {
         }
         return score;
     }
-    public static void main(String[] args) {
-        try {
-            String path = "c:\\users\\xdavi\\documents\\temp\\data01.txt";
-            BufferedReader read = new BufferedReader(new FileReader(path));
-            String ln = read.readLine();
-            System.out.printf("part 1 = %d\n", solve(ln));
-        } catch(FileNotFoundException e) {
-        } catch(IOException e) {
-        }
+    public static void main(String[] args) throws IOException {
+        var ln = Files.readString(Path.of(
+                "/Users", "xdavidliu", "Documents", "temp", "data01.txt"));
+        System.out.printf("part 1 = %d\n", solve(ln));
     }
 }
