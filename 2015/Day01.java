f816aabd64273dc359404f07d9bc0b71524c6075
David Liu <xdavidliu@gmail.com>
2022-08-20 06:58:06 -0400

    Update Day01.java

--- 2015/Day01.java
+++ 2015/Day01.java
@@ -1,11 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Day01 {
-    public static void main(String[] args) throws java.io.IOException {
-        String input = java.nio.file.Files.readString(java.nio.file.Path.of(
-                "/Users/xdavidliu/Documents/temp/data01.txt"));
-        int score = 0, firstNeg = -1;
+    public static void main(String[] args) throws IOException {
+        String input = Files.readString(Path.of(
+                "/home/xdavidliu/Documents/temp/data01.txt"));
+        int score = 0;
+        Integer firstNeg = null;
         for (int i = 0; i < input.length(); ++i) {
             switch(input.charAt(i)) { case '(' -> ++score; case ')' -> --score; }
-            if (firstNeg == -1 && score == -1) firstNeg = i + 1;
+            if (firstNeg == null && score == -1) firstNeg = i + 1;
         }
         System.out.format("part 1 = %d, part 2 = %d\n", score, firstNeg);
     }
