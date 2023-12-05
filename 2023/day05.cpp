1319cad32e685f51847da50bfcaac2445c3f0b0a
David Liu <xdavidliu@gmail.com>
2023-12-05 13:23:03 -0500

    Update day05.cpp

--- 2023/day05.cpp
+++ 2023/day05.cpp
@@ -47,7 +47,7 @@ long location(const long seed, const std::vector<std::vector<Row>> &maps) {
     return value;
 }
 
-void solve(const std::vector<long> &seeds, const std::vector<std::vector<Row>> &maps) {
+void part1(const std::vector<long> &seeds, const std::vector<std::vector<Row>> &maps) {
     auto small = std::numeric_limits<long>::max();
     for (const auto &seed : seeds) {
         small = std::min(small, location(seed, maps));
@@ -55,8 +55,45 @@ void solve(const std::vector<long> &seeds, const std::vector<std::vector<Row>> &
     std::cout << "part 1 = " << small << '\n';  // 84470622
 }
 
+void match_row(std::vector<std::pair<long, long>> &out, const long source, const long range, const Row &row) {
+  const auto left = std::max(source, row.source);
+  const auto right = std::min(source + range, row.source + row.range);
+  if (left <= right) {
+    const auto out_left = left + row.destination - row.source;
+    const auto out_right = right + row.destination - row.source;
+    out.emplace_back(out_left, out_right);
+  }
+}
+
+void match_map(std::vector<std::pair<long, long>> &out, const long source, const long range, const std::vector<Row> &map) {
+  for (const auto &row : map) {
+    match_row(out, source, range, row);
+  }
+}
+
+/*
+ * wait this is wrong
+ * must match all parts
+ * */
+
+void part2(const std::vector<long> &seeds, const std::vector<std::vector<Row>> &maps) {
+  std::vector<std::pair<long, long>> pairs_from, pairs_to;
+  for (std::size_t i = 0; i < seeds.size(); i += 2) {
+    pairs_from.emplace_back(seeds[i], seeds[i+1]);
+  }
+  for (const auto &map : maps) {
+    pairs_to.clear();
+    for (const auto &[source, range] : pairs_from) {
+      match_map(pairs_to, source, range, map);
+    }
+    pairs_to.swap(pairs_from);
+  }
+  const auto ans = std::min_element(pairs_from.cbegin(), pairs_from.cend())->first;
+  std::cout << "part 2 = " << ans << '\n';  // too high
+}
+
 int main() {
-    const char path[] = "/home/xdavidliu/Documents/temp/data.txt";
+    const char path[] = "/usr/local/google/home/xdavidliu/Documents/temp/example.txt";
     if (auto fs = std::ifstream(path)) {
         std::string line;
         std::getline(fs, line);  // line.substr(2 + line.find(": "))
@@ -68,6 +105,7 @@ int main() {
         std::getline(fs, line);  // empty
         std::vector<std::vector<Row>> maps;
         while (fs) { maps.push_back(read_rows(fs)); }
-        solve(seeds, maps);
+        part1(seeds, maps);
+        part2(seeds, maps);
     }
 }
