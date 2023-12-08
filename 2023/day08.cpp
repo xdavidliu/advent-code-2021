489b2678007687e734b123e4363657b95645222c
David Liu <xdavidliu@gmail.com>
2023-12-08 06:52:51 -0500

    Update day08.cpp

--- 2023/day08.cpp
+++ 2023/day08.cpp
@@ -14,58 +14,58 @@ insert(std::string_view line, std::map<std::string, std::string> &left, std::map
     right[std::string(key)] = std::string(right_val);
 }
 
-class State {
+class Desert {
     const std::string instructions;
-    std::string place;
     const std::map<std::string, std::string> left, right;
-    std::size_t ind;
 public:
     // https://stackoverflow.com/a/23931920/2990344
-    State(std::string instructions,
-          std::string place,
+    Desert(std::string instructions,
           std::map<std::string, std::string> left,
           std::map<std::string, std::string> right)
             :
             instructions(std::move(instructions)),
-            place(std::move(place)),
             left(std::move(left)),
-            right(std::move(right)),
-            ind(0) {}
-
-    std::size_t count_steps() {
-        std::size_t steps = 0;
-        while (place != "ZZZ") {
-            switch (instructions[ind]) {
-                case 'L' : {
-                    place = left.at(place);
-                    break;
-                }
-                case 'R' : {
-                    place = right.at(place);
-                    break;
-                }
+            right(std::move(right))
+            {}
+    void update(std::string &place, std::size_t &ind) const {
+        switch (instructions[ind]) {
+            case 'L' : {
+                place = left.at(place);
+                break;
             }
-            ind = (ind + 1) % instructions.size();
+            case 'R' : {
+                place = right.at(place);
+                break;
+            }
+        }
+        ind = (ind + 1) % instructions.size();
+    }
+    // clang suggested nodiscard; whatever
+    [[nodiscard]] std::size_t count_steps_until(std::string_view start, std::string_view end) const {
+        std::string place(start);
+        std::size_t steps = 0, ind = 0;
+        while (place != end) {
+            update(place, ind);
             ++steps;
         }
         return steps;
     }
 };
 
-State read(const char *path) {
+Desert read(const char *path) {
     if (auto fs = std::ifstream(path)) {
         std::string instructions, line;
         std::getline(fs, instructions);
         std::getline(fs, line);  // empty
         std::map<std::string, std::string> left, right;
         while (std::getline(fs, line)) { insert(line, left, right); }
-        return {std::move(instructions), "AAA", std::move(left), std::move(right)};
+        return {std::move(instructions), std::move(left), std::move(right)};
     } else {
         throw std::exception();
     }
 }
 
 int main() {
-    auto state = read("/home/xdavidliu/Documents/temp/data.txt");
-    std::cout << "part 1 = " << state.count_steps() << '\n';  // 19241
+    auto desert = read("/home/xdavidliu/Documents/temp/data.txt");
+    std::cout << "part 1 = " << desert.count_steps_until("AAA", "ZZZ") << '\n';  // 19241
 }
