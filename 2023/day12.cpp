7212fb5b88f7c241535a27e4d1f78e93f5e12c97
David Liu <xdavidliu@gmail.com>
2023-12-12 21:09:55 -0500

    Update day12.cpp

--- 2023/day12.cpp
+++ 2023/day12.cpp
@@ -1,103 +1,145 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <exception>
 #include <fstream>
 #include <algorithm>
-#include <cmath>
+#include <map>
 
-// hypothesis; it's just manhattan distance
+constexpr int none = -1;
 
-bool is_dot(const char ch) { return ch == '.'; }
+// todo idea: check if remaining has enough # and ? to work
+// do it in O(1) time by tracking remaining . and ? in register
+// if not, terminate instantly
+// DP? store table of answers?
 
-long count_between(const std::size_t left, const std::size_t right, const std::vector<std::size_t> &indices) {
-  if (left > right) {
-    return count_between(right, left, indices);
-  }
-  const auto first_empty_between = std::upper_bound(indices.cbegin(), indices.cend(), left);
-  const auto first_empty_after = std::upper_bound(indices.cbegin(), indices.cend(), right);
-  return static_cast<long>(first_empty_after - first_empty_between);
-}
-
-std::vector<std::string> read_grid(const char *filepath) {
-  if (auto fs = std::ifstream(filepath)) {
-    std::vector<std::string> grid;
-    std::string line;
-    while (std::getline(fs, line)) {
-      grid.push_back(line);
-    }
-    return grid;
-  } else {
-    throw std::exception();
-  }
-}
-
-std::vector<std::size_t> find_empty_rows(const std::vector<std::string> &grid) {
-  std::vector<std::size_t> empty_rows;
-  for (std::size_t r = 0; r < grid.size(); ++r) {
-    if (std::all_of(grid[r].cbegin(), grid[r].cend(), is_dot)) {
-      empty_rows.push_back(r);
-    }
-  }
-  return empty_rows;
-}
-
-std::vector<std::size_t> find_empty_cols(const std::vector<std::string> &grid) {
-  std::vector<std::size_t> empty_cols;
-  for (std::size_t c = 0; c < grid.front().size(); ++c) {
-    bool col_empty = true;
-    for (std::size_t r = 0; r < grid.size(); ++r) {
-      if (!is_dot(grid[r][c])) {
-        col_empty = false;
-        break;
-      }
-    }
-    if (col_empty) {
-      empty_cols.push_back(c);
-    }
-  }
-  return empty_cols;
-}
-
-std::vector<std::pair<std::size_t, std::size_t>> find_galaxies(const std::vector<std::string> &grid) {
-  std::vector<std::pair<std::size_t, std::size_t>> galaxies;
-  for (std::size_t r = 0; r < grid.size(); ++r) {
-    for (std::size_t c = 0; c < grid.front().size(); ++c) {
-      if (grid[r][c] == '#') {
-        galaxies.emplace_back(r, c);
-      }
-    }
-  }
-  return galaxies;
-}
-
-long solve_with_multiplier(
-    const std::vector<std::pair<std::size_t, std::size_t>> &galaxies,
-    const std::vector<std::size_t> &empty_rows,
-    const std::vector<std::size_t> &empty_cols,
-    const std::size_t empty_multiplier)
+long collect(
+        const std::string &grid,
+        int i_grid,
+        const std::vector<int> &sizes,
+        int i_size,
+        int remain,
+        std::map<std::tuple<int, int, int>, long> &table)
 {
-  long sum = 0;
-  for (std::size_t i = 0; i+1 < galaxies.size(); ++i) {
-    const auto [ri, ci] = galaxies[i];
-    for (std::size_t k = i+1; k < galaxies.size(); ++k) {
-      const auto [rk, ck] = galaxies[k];
-      sum += std::abs(static_cast<long>(rk) - static_cast<long>(ri));
-      sum += std::abs(static_cast<long>(ck) - static_cast<long>(ci));
-      sum += (empty_multiplier - 1) * count_between(ri, rk, empty_rows);
-      sum += (empty_multiplier - 1) * count_between(ci, ck, empty_cols);
+    const auto key = std::make_tuple(i_grid, i_size, remain);
+    const auto found = table.find(key);
+    if (found != table.cend()) { return found->second; }
+    char ch;
+    while (i_size != sizes.size() && i_grid != grid.size() && '?' != (ch = grid[i_grid])) {
+        ++i_grid;
+        if (ch == '.') {
+            if (remain == none) {
+                // do nothing
+            } else if (remain == 0) {  // just finished a batch of #
+                ++i_size;
+                remain = none;
+            } else {  // remain > 0
+                table[key] = 0;
+                return 0;  // impossible, so terminate
+            }
+        } else {  // ch == '#'
+            if (remain == none) {
+                remain = sizes[i_size] - 1;
+            } else if (remain == 0) {
+                table[key] = 0;
+                return 0;  // impossible
+            } else {  // remain > 0
+                --remain;
+            }
+        }
     }
-  }
-  return sum;
+    if (i_size == sizes.size()) {
+        for (int i = i_grid; i < grid.size(); ++i) {
+            if (grid[i] == '#') {
+                table[key] = 0;
+                return 0;
+            }
+        }
+        table[key] = 1;
+        return 1;
+    } else if (i_grid == grid.size()) {
+        if ( remain == 0 && i_size + 1 == sizes.size()) {
+            table[key] = 1;
+            return 1;
+        }
+    } else {  // ch == '?' and haven't finished yet
+        // try ? = #
+        long total = 0;
+        if (remain == none) {
+            const auto new_remain = sizes[i_size] - 1;
+            total += collect(grid, 1 + i_grid, sizes, i_size, new_remain, table);
+        } else if (remain > 0) {
+            total += collect(grid, 1 + i_grid, sizes, i_size, remain - 1, table);
+        }
+        // try ? = .
+        if (remain == 0) {
+            total += collect(grid, 1 + i_grid, sizes, 1 + i_size, none, table);
+        } else if (remain == none) {
+            total += collect(grid, 1 + i_grid, sizes, i_size, none, table);
+        }
+        table[key] = total;
+        return total;
+    }
+    table[key] = 0;
+    return 0;
+}
+
+long solve(const std::string &grid, const std::vector<int> &sizes) {
+    std::map<std::tuple<int, int, int>, long> table;
+    return collect(grid, 0, sizes, 0, none, table);
+}
+
+struct Problem {
+    std::string grid;
+    std::vector<int> nums;
+};
+
+auto split_nums(const std::string &list) {
+    std::size_t i = 0;
+    std::vector<int> nums;
+    while (true) {
+        const auto k = list.find(',', i);
+        if (k == std::string::npos) {
+            nums.push_back(std::stoi(list.substr(i)));
+            break;
+        } else {
+            nums.push_back(std::stoi(list.substr(i, k-i)));
+            i = k + 1;
+        }
+    }
+    return nums;
+}
+
+auto times_five_with_questions(const std::string &text) {
+    std::string out = text;
+    for (int i = 0; i < 4; ++i) {
+        out.push_back('?');
+        std::copy(text.cbegin(), text.cend(), std::back_inserter(out));
+    }
+    return out;
+}
+
+auto times_five(const std::vector<int> &sizes) {
+    std::vector<int> out = sizes;
+    for (int i = 0; i < 4; ++i) {
+        std::copy(sizes.cbegin(), sizes.cend(), std::back_inserter(out));
+    }
+    return out;
 }
 
 int main() {
-  const auto grid = read_grid("/tmp/data.txt");
-  const auto empty_rows = find_empty_rows(grid);
-  const auto empty_cols = find_empty_cols(grid);
-  const auto galaxies = find_galaxies(grid);
-  const auto part1 = solve_with_multiplier(galaxies, empty_rows, empty_cols, 2);
-  std::cout << "part 1 = " << part1 << '\n';  // 9599070
-  const auto part2 = solve_with_multiplier(galaxies, empty_rows, empty_cols, 1000000);
-  std::cout << "part 2 = " << part2 << '\n';  // 842645913794
+    std::vector<Problem> problems;
+    if (auto fs = std::ifstream("/home/xdavidliu/Documents/temp/data.txt")) {
+        long part1 = 0, part2 = 0;
+        std::string grid, nums;
+        while (fs >> grid) {
+            fs >> nums;
+            const auto sizes = split_nums(nums);
+            part1 += solve(grid, sizes);
+            const auto five_grid = times_five_with_questions(grid);
+            const auto five_sizes = times_five(sizes);
+            part2 += solve(five_grid, five_sizes);
+        }
+        std::cout << "part 1 = " << part1 << '\n';  // 7017
+        std::cout << "part 2 = " << part2 << '\n';  // 527570479489
+    }
 }
