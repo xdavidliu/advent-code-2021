141e8275549638b28d12d9e8362b19872ac45dd2
David Liu <xdavidliu@gmail.com>
2023-12-15 12:49:51 -0500

    Update day15_partial.cpp

--- 2023/day15_partial.cpp
+++ 2023/day15_partial.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <string_view>
+#include <algorithm>
 
 int hash(const std::string_view instruction) {
   int current = 0;
@@ -25,13 +26,54 @@ auto split_commas(const std::string &line) {
   return out;
 }
 
+struct Lens {
+  std::string label;
+  int length;
+};
+
+void perform(const std::string ins, std::vector<std::vector<Lens>> &boxes) {
+  const bool is_minus = ins.back() == '-';
+  const auto op_pos = ins.find(is_minus ? '-' : '=');
+  const auto label = ins.substr(0, op_pos);
+  const auto label_hsh = hash(label);
+  auto &box = boxes[label_hsh];
+  const auto matches = [&label] (const Lens &lens) { return lens.label == label; };
+  if (ins.back() == '-') {
+    std::remove_if(box.begin(), box.end(), matches);
+  } else {  // =
+    const auto focal_str = ins.substr(1 + op_pos, ins.size() - 1 - op_pos);
+    const auto focal = std::stoi(focal_str);
+
+    auto found = std::find_if(box.begin(), box.end(), matches);
+    if (found != box.end()) {
+      found->length = focal;
+    } else {
+      box.push_back({label, focal});
+    }
+  }
+}
+
 int main() {
-  if (auto fs = std::ifstream("/tmp/data.txt")) {
+  if (auto fs = std::ifstream("/tmp/example.txt")) {
     std::string line;
     std::getline(fs, line);
     const auto tokens = split_commas(line);
-    long part1 = 0;
-    for (const auto &token : tokens) { part1 += hash(token); }
-    std::cout << "part 1 = " << part1 << '\n';
+    std::vector<std::vector<Lens>> boxes(256);
+    long part1 = 0, part2 = 0;
+    for (const auto &token : tokens) {
+      const auto hsh = hash(token);
+      part1 += hsh;
+      perform(token, boxes);
+    }
+    std::cout << "part 1 = " << part1 << '\n';  // 521341
+    for (std::size_t i_box = 0; i_box < boxes.size(); ++i_box) {
+      const auto &box = boxes[i_box];
+      for (std::size_t i_lens = 0; i_lens < box.size(); ++i_lens) {
+        std::cout << box[i_lens].length << ' ';
+        part2 += (1 + i_box) * (1 + i_lens) * box[i_lens].length;
+      }
+      if (!box.empty()) { std::cout << " | " << i_box << '\n'; }
+    }
+    std::cout << "part 2 = " << part2 << '\n';
   }
 }
