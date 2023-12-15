050dc396b50545d969f627a088a0da7883345d34
David Liu <xdavidliu@gmail.com>
2023-12-15 07:32:32 -0500

    Update day14_partial.cpp

--- 2023/day14_partial.cpp
+++ 2023/day14_partial.cpp
@@ -5,44 +5,53 @@
 #include <exception>
 #include <algorithm>
 
-char no_circle(const char ch) {
-    return ch == 'O' ? '.' : ch;
-}
-
-std::string line_with_no_circles(const std::string &line) {
-    std::string out;
-    std::transform(line.cbegin(), line.cend(), std::back_inserter(out), no_circle);
-    return out;
-}
-
-std::vector<std::string> with_no_circles(const std::vector<std::string> &grid) {
-    std::vector<std::string> out;
-    std::transform(grid.cbegin(), grid.cend(), std::back_inserter(out), line_with_no_circles);
-    return out;
-}
-
-void drop(const std::vector<std::string> &grid, std::vector<std::string> &other, const std::size_t col) {
+void drop(std::vector<std::string> &grid, const std::size_t col) {
     std::size_t src = 1, dest = 0;
-    while (true) {  // ???
-        if (grid[dest][col] == ) {
-
+    while (dest + 1 < grid.size() && src < grid.size()) {
+        if (grid[dest][col] != '.') {
+            ++dest;
+            src = dest + 1;
+        } else if (grid[src][col] == '#') {
+            dest = src + 1;
+            src = dest + 1;
+        } else if (grid[src][col] == '.') {
+            ++src;
+        } else {  // src is 'O'
+            grid[dest][col] = 'O';
+            grid[src][col] = '.';
+            ++dest;
+            src = dest + 1;
         }
-
     }
-
-
 }
 
-void solve(const std::vector<std::string> &grid) {
-    auto other = with_no_circles(grid);
-    for (std::size_t col = 0; col < grid.front().size(); ++col) {
-        drop(grid, other, col);
+void print(std::vector<std::string> &grid) {
+    for (const auto &line : grid) {
+        std::cout << line << '\n';
     }
-    // todo: sum and print answer
+}
+
+auto tally(std::vector<std::string> &grid) {
+    std::size_t out = 0;
+    for (std::size_t row = 0; row < grid.size(); ++row) {
+        std::size_t count = 0;
+        for (const auto &ch : grid[row]) {
+            if (ch == 'O') { ++count; }
+        }
+        out += count * (grid.size() - row);
+    }
+    return out;
+}
+
+void solve(std::vector<std::string> &grid) {
+    for (std::size_t col = 0; col < grid.front().size(); ++col) {
+        drop(grid, col);
+    }
+    std::cout << "part 1 = " << tally(grid) << '\n';  // 109385
 }
 
 void foo1() {
-    constexpr char filepath[] = "/home/xdavidliu/Documents/temp/example.txt";
+    constexpr char filepath[] = "/home/xdavidliu/Documents/temp/data.txt";
     std::vector<std::string> grid;
     if (auto fs = std::ifstream(filepath)) {
         std::string line;
@@ -53,7 +62,6 @@ void foo1() {
     } else {
         throw std::exception();
     }
-    std::cout << grid.size();
 }
 
 int main() {
