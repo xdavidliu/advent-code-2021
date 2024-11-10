82b83b7f5b8714935fbdcca167c8d4a37de1c593
David Liu <xdavidliu@gmail.com>
2024-11-10 09:20:39 -0500

    Update day07.lisp

--- 2019/day07.lisp
+++ 2019/day07.lisp
@@ -1,7 +1,7 @@
-(load "~/Documents/intcode.lisp")
+(load "~/Documents/aoc/intcode.lisp")
 (import '(new-computer run-once computer-input computer-output))
 
-(load "~/Documents/util.lisp")
+(load "~/Documents/aoc/util.lisp")
 (defparameter *input-vec* (read-input "~/Documents/aoc/input07.txt"))
 
 (defun nc (phase)
