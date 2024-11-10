d518aab2bdc57c0368404710e52f511ed5f3fc6b
David Liu <xdavidliu@gmail.com>
2024-11-10 09:21:07 -0500

    Update day02.lisp

--- 2019/day02.lisp
+++ 2019/day02.lisp
@@ -1,7 +1,7 @@
-(load "~/Documents/intcode.lisp")
+(load "~/Documents/aoc/intcode.lisp")
 (import '(new-computer apply-words run zeroth-val))
 
-(load "~/Documents/util.lisp")
+(load "~/Documents/aoc/util.lisp")
 (defparameter *input-vec* (read-input "~/Documents/aoc/input02.txt"))
 
 (let ((cmp (new-computer *input-vec*)))
