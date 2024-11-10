be847509b99ee04037562a9fecdd84e80a198573
David Liu <xdavidliu@gmail.com>
2024-11-10 09:26:42 -0500

    Update util.lisp

--- 2019/util.lisp
+++ 2019/util.lisp
@@ -15,3 +15,40 @@
     (let ((toks (split (read-line strm) #\,)))
       (coerce (mapcar #'parse-integer toks)
 	      'vector))))
+
+;; permutations functions
+
+;; assumes vec elems unique
+(defun bound-index (vec)
+  (do ((i (1- (length vec)) (1- i)))
+      ((or (zerop i)
+	   (< (elt vec (1- i))
+	      (elt vec i)))
+       i)))
+
+;; assumes there's some elem > piv in vec
+(defun successor-index (piv vec)
+  (do ((i (1- (length vec)) (1- i)))
+      ((< piv (elt vec i))
+       i)))
+
+(defun swap-vec-elem (vec i j)
+  (if (/= i j)
+      (let ((xi (elt vec i))
+	    (xj (elt vec j)))
+	(setf (elt vec i) xj)
+	(setf (elt vec j) xi))))
+
+(defun reverse-from (vec i)
+  (do ((l i (1+ l))
+       (r (1- (length vec)) (1- r)))
+      ((>= l r))
+    (swap-vec-elem vec l r)))
+
+(defun next-perm (vec)
+  (let ((bi (bound-index vec)))
+    (and (not (zerop bi))
+	 (let ((si (successor-index (elt vec (1- bi)) vec)))
+	   (swap-vec-elem vec (1- bi) si)
+	   (reverse-from vec bi)
+	   t))))
