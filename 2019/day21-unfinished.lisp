e3f00ca0bb1c8ce970c776e8c7ed3fb6a2193f72
David Liu <xdavidliu@gmail.com>
2024-11-26 20:26:40 -0500

    Update day21-unfinished.lisp

--- 2019/day21-unfinished.lisp
+++ 2019/day21-unfinished.lisp
@@ -1,11 +1,6 @@
 (load "~/Documents/aoc/util.lisp")
 (load "~/Documents/aoc/intcode.lisp")
 
-(defparameter ascii-prog
-"NOT D J
-WALK
-")
-
 (defun run (ascii-prog)
   (let* ((input-vec (read-input "~/Documents/aoc/input21.txt"))
 	 (cmp (new-computer input-vec))
@@ -13,9 +8,55 @@ WALK
     (setf (computer-input cmp) (car in-prog)
 	  in-prog (cdr in-prog))
     (do (outs (prev t))
-        ((null prev) (map 'string 'code-char (nreverse outs)))
+        ((null prev)
+	 (let ((first-out (car outs)))
+	   (if (> first-out 256)
+	       first-out
+	       (map 'string 'code-char (nreverse outs)))))
       (case (setf prev (run-til-io-or-end cmp))
 	(in (when in-prog
 	      (setf (computer-input cmp) (car in-prog)
 		    in-prog (cdr in-prog))))
 	(out (push (computer-output cmp) outs))))))
+
+#|
+(run "WALK
+")
+
+shows that one hole is ##.##
+
+(run "NOT A J
+WALK
+")
+
+##.#..###
+
+(run "NOT B J
+AND A J
+WALK
+")
+
+###...##
+
+Solution:
+
+V
+#...#        
+
+V   V
+###.#..#        (D and (not C)) or (not A)
+
+V   V
+###.#.#
+
+|#
+
+
+
+(format t "part 1 = ~A~%" (run "NOT C T
+AND D T
+NOT A J
+OR T J
+WALK
+"))
+;; 19354818
