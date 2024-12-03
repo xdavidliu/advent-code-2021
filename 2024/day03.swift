e29752ca834d615a7387618a1e8ec84d2a6006ff
David Liu <xdavidliu@gmail.com>
2024-12-03 11:11:15 -0500

    Update day03.swift

--- 2024/day03.swift
+++ 2024/day03.swift
@@ -1,35 +1,25 @@
-// broken version that segfaults
-
 import Foundation
 
-func getLines(_ fileName: String) -> [String.SubSequence] {
-    do {
-        let url = URL(fileURLWithPath: fileName)
-        let text = try String(contentsOf: url, encoding: .utf8)
-        return text.split(separator: "\n")
-    } catch {
-        return []
+let re = /mul\((\d{1,3}),(\d{1,3})\)/
+
+func computeTotal(_ text: String) -> Int {
+    var total = 0
+    for m in text.matches(of: re) {
+        total += Int(m.1)! * Int(m.2)!
     }
+    return total
 }
 
-let lines = getLines("/Users/xdavidliu/input03.txt")
-//let p1 = lines.map{ computeTotal($0, ignoreDo: true) }.reduce(0, +)
-let p1 = lines.map{ simpleComputeTotal($0) }.reduce(0, +)
-// let p2 = lines.map{ computeTotal($0, ignoreDo: false) }.reduce(0, +)
-print("part 1 = \(p1)")  // 183380722
-// print("part 2 = \(p2)")  // 183380722
-
-let mulDoRe = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/
-
-func computeTotal(_ text: String.SubSequence, ignoreDo: Bool) -> Int {
+let doRe = /do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/
+func computeDoTotal(_ text: String) -> Int {
     var total = 0
     var effect = true
-    for m in text.matches(of: mulDoRe) {
+    for m in text.matches(of: doRe) {
         if m.0 == "do()" {
             effect = true
         } else if m.0 == "don't()" {
             effect = false
-        } else if ignoreDo || effect {
+        } else if effect {
             if m.0.starts(with: "mul") {
                 total += Int(m.1!)! * Int(m.2!)!
             }
@@ -38,14 +28,19 @@ func computeTotal(_ text: String.SubSequence, ignoreDo: Bool) -> Int {
     return total
 }
 
-let mulRe = /mul\((\d{1,3}),(\d{1,3})\)/
-let re = /mul\(.+\)/
-
-func simpleComputeTotal(_ text: String.SubSequence) -> Int {
-    var total = 0
-    for m in text.matches(of: re) {
-        
-//        total += Int(m.1)! * Int(m.2)!
+func getLines(_ fileName: String) -> [String] {
+    do {
+        let url = URL(fileURLWithPath: fileName)
+        let text = try String(contentsOf: url, encoding: .utf8)
+        return text.split(separator: "\n").map(String.init)
+    } catch {
+        return []
     }
-    return total
 }
+
+let text = getLines("/Users/xdavidliu/input03.txt").joined()
+// join because effect crosses over across lines
+let p1 = computeTotal(text)
+let p2 = computeDoTotal(text)
+print("part 1 = \(p1)")  // 183380722
+print("part 2 = \(p2)")  // 82733683
