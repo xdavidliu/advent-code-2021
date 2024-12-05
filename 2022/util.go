6c749b4c36896f6c0d0b992853e2fa92a825ec4b
David Liu <xdavidliu@gmail.com>
2024-12-05 17:53:02 -0500

    Update util.go

--- 2022/util.go
+++ 2022/util.go
@@ -17,3 +17,39 @@ func sign(x int) int {
 		return 0
 	}
 }
+
+type queue struct {
+	front []int
+	back  []int
+}
+
+func pop[T](a *[]int) T {
+	end := len(*a) - 1
+	x := (*a)[end]
+	*a = (*a)[:end]
+	return x
+}
+
+func push(x int, a *[]int) {
+	(*a) = append(*a, x)
+}
+
+func makeQueue() queue {
+	return queue{nil, nil}
+}
+
+func (q *queue) add(x int) {
+	q.back = append(q.back, x)
+}
+
+func (q *queue) remove() int {
+	if 0 == len(q.front) {
+		for 1 < len(q.back) {
+			end := len(q.back) - 1
+			x := q.back[end]
+			q.back = q.back[:end]
+			q.front = append(q.front, x)
+		}
+	}
+}
+
