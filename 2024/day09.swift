8d7ff9f0e1342ba91283858ce0de8a447115d863
David Liu <xdavidliu@gmail.com>
2024-12-09 18:20:23 -0500

    Update and rename day09-part1.swift to day09.swift

--- 2024/day09.swift
+++ 2024/day09.swift
@@ -1,19 +1,17 @@
 import Foundation
 
-let filename = "/Users/xdavidliu/sample.txt"
+let filename = "/Users/xdavidliu/input09.txt"
 let diskMap = readDiskMap(filename)
 let fileData = convertToFileData(diskMap)
 print("part 1 =", part1(fileData))  // 6258319840548
-print("part 2 =", part2(diskMap))  //
-// 10690294388280 too high
-// 2318
-// expect 2858
+print("part 2 =", part2(diskMap))  // 6286182965311
 
 func sumFormula(_ n: Int) -> Int {
     return n * (n+1) / 2
 }
 
-func popRightmostAtMost(_ length: Int, _ table: inout [[Int]]) -> Optional<Int> {
+func popRightmostAtMost(_ length: Int, _ table: inout [[Int]])
+-> Optional<Int> {
     var rightMost = Int.min
     var rightMostInd: Optional<Int> = nil
     for i in 1...length {
@@ -54,20 +52,21 @@ func part2(_ diskMap: [Int]) -> Int {
         if left.isMultiple(of: 2) {
             if !disabled[left] {
                 let addr = left / 2
-//                printVal(val: addr, n: length)
                 total += addr * (sumFormula(dataCount - 1 + length) - sumFormula(dataCount - 1))
+                disabled[left] = true
             } else {
-//                printDots(n: length)
             }
             dataCount += length
         } else {
             var freeRemain = length
             while freeRemain > 0 {
                 if let right = popRightmostAtMost(freeRemain, &table) {
+                    if right < left {
+                        break
+                    }
                     let rightLength = diskMap[right]
                     disabled[right] = true
                     let addr = right / 2
-//                    printVal(val: addr, n: rightLength)
                     total += addr * (sumFormula(dataCount - 1 + rightLength) - sumFormula(dataCount - 1))
                     dataCount += rightLength
                     freeRemain -= rightLength
@@ -75,33 +74,12 @@ func part2(_ diskMap: [Int]) -> Int {
                     break
                 }
             }
-            if freeRemain > 0 {
-//                printDots(n: freeRemain)
-            }
             dataCount += freeRemain
         }
         left += 1
     }
-//    print()
     return total
 }
-
-/*
- ah, address can be obtained by index in diskMap. That's done BEFORE the moving
- from ind, what's addr
- ind is even number. 0, 2, 4. Ah, it's just addr = ind / 2
- 
- sum from x to x + y
- 2 + 3 + 4 + 5
- = sum to 5 - sum to (2-1)
- = 5(6)/2 - 1(2)/2
- = 15 - 1 = 14
- 
- (x+y)(x+y+1)/2 - x(x-1)/2
- 
- length is the value, so track how many seen so far.
- 
- */
 
 func makeTable(_ diskMap: [Int]) -> [[Int]] {
     var out = [[Int]](repeating: [], count: 10)
@@ -157,23 +135,3 @@ func part1(_ fileData: [Int]) -> Int {
     }
     return acc
 }
-
-
-/*
-
- have list with key being from 1-9 and value being list of indices
- go thru input and for each data (not free) so odd index with length x, append to
- the x-th slot in the top list
- 
- now go thru each free. Suppose length y. Then want rightmost index of z <= y.
- Note this rightmost index must be > current left.
- search thru list for that, in O(1) time. pop that one, now free is y - z. Repeat
- until the free is 0 or there does not exist ANY z satisfying this.
- 
- Then move on to next free, and repeat.
- 
- while this is being done, accumulate score. Keep track of which right ones are moved,
- that way you can omit them when summing.
- 
- 
- */
