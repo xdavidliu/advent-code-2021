cd74c313ea1f09c764bfe629d72be05d0b83bd0b
David Liu <xdavidliu@gmail.com>
2024-12-13 16:51:28 -0500

    Update and rename day07-part1.jl to day07.jl

--- 2020/day07.jl
+++ 2020/day07.jl
@@ -9,7 +9,7 @@ function parseline(line)
     bag, containtext = split(chop(line), " bags contain ")
     containlist = containtext == "no other bags" ?
         [] : [parselistitem(s) for s in split(containtext, ", ")]
-    bag, containlist
+    String(bag), containlist
 end
 
 getentries(filename) = [parseline(x) for x in readlines(filename)]
@@ -25,10 +25,10 @@ function populatetable(entries)
     tab
 end
 
-function dfs(bag, table, seen)
+function dfs1(bag, table, seen)
     for other in get(table, bag, [])
         push!(seen, other)
-        dfs(other, table, seen)
+        dfs1(other, table, seen)
     end
 end
 
@@ -36,8 +36,26 @@ function part1()
     entries = getentries("/Users/xdavidliu/Documents/input07.txt")
     tab = populatetable(entries)
     seen = Set{String}()
-    dfs("shiny gold", tab, seen)
+    dfs1("shiny gold", tab, seen)
     println("part 1 = ", length(seen))
 end
 
+function dfs2(bag, entriesdict)
+    total = 0
+    ps = get(entriesdict, bag, [])
+    for (c, other) in ps
+        total += c * dfs2(other, entriesdict)
+    end
+    total + 1  # for bag itself
+end
+
+function part2()
+    entries = getentries("/Users/xdavidliu/Documents/input07.txt")
+    entriesdict = Dict(entries)
+    tab = populatetable(entries)
+    # -1 to exclude shiny gold itself
+    println("part 2 = ", -1 + dfs2("shiny gold", entriesdict))
+end
+
 part1()  # 192
+part2()  # 12128
