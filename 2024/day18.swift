1c5fe33c34c0f212aebcaa1f8faa61257a02ba34
David Liu <xdavidliu@gmail.com>
2024-12-18 10:26:54 -0500

    Update and rename day18-part1.swift to day18.swift

--- 2024/day18.swift
+++ 2024/day18.swift
@@ -4,12 +4,20 @@ func emptyGrid(_ n: Int) -> [[UInt8]] {
     return makeGrid(elem: byteOf("."), nr: n, nc: n)
 }
 
-func makeInsertions(_ grid: inout [[UInt8]], _ filename: String, toInsert: Int) {
-    var insCount = 0
+func getPoints(_ filename: String) -> [(Int, Int)] {
+    var out: [(Int, Int)] = []
     for line in getLines(filename) {
         let words = line.split(separator: ",")
         let r = Int(words[1])!
         let c = Int(words[0])!
+        out.append((r, c))
+    }
+    return out
+}
+
+func makeInsertions(_ grid: inout [[UInt8]], _ points: [(Int, Int)], toInsert: Int) {
+    var insCount = 0
+    for (r, c) in points {
         grid[r][c] = byteOf("#")
         insCount += 1
         if insCount == toInsert {
@@ -18,13 +26,34 @@ func makeInsertions(_ grid: inout [[UInt8]], _ filename: String, toInsert: Int) 
     }
 }
 
-let filename = "/Users/xdavidliu/input18.txt"
-let n = 71
-let toInsert = 1024
+let filename = "/Users/xdavidliu/input18.txt"  // input18
+let points = getPoints(filename)
+let n = 71  // 71 and 7
+let toInsertPart1 = 1024  // 12 and 1024
 
-var grid = emptyGrid(n)
-makeInsertions(&grid, filename, toInsert: toInsert)
-print("part 1 =", bfs(grid))  // 436
+func part1() {
+    var grid = emptyGrid(n)
+    makeInsertions(&grid, points, toInsert: toInsertPart1)
+    print("part 1 =", bfs(grid))  // 436
+}
+
+// manually binary search this until find last toInsert that "works"
+func part2() {
+    var grid = emptyGrid(n)
+//    print(points.count)  // 3450
+    makeInsertions(&grid, points, toInsert: 2960)
+    if -1 == bfs(grid) {
+        print("does not work")
+    } else {
+        print("works")
+    }
+}
+
+part1()
+//part2()
+
+let p2 = points[2960]
+print("part 2 = ", p2.1, ",", p2.0, separator: "")  // 61,50
 
 func bfs(_ grid: [[UInt8]]) -> Int {
     var que = Queue<(Int, Int, Int)>()
@@ -51,5 +80,5 @@ func bfs(_ grid: [[UInt8]]) -> Int {
             seen.insert(ind)
         }
     }
-    fatalError("bfs")
+    return -1
 }
