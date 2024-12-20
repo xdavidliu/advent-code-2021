4fc96b8daf017ff52b4d33c05da45a7552035025
David Liu <xdavidliu@gmail.com>
2024-12-20 06:54:13 -0500

    Update day20-part1.jl

--- 2020/day20-part1.jl
+++ 2020/day20-part1.jl
@@ -9,8 +9,13 @@ function readproblem(filename)
     Dict(getgrid(b) for b in blocks)
 end
 
+function column(grid, right)
+    right ? String([q[end] for q in grid]) : String([q[1] for q in grid])
+end
+
 function edges(grid)
-    [grid[1], grid[end], String([q[end] for q in grid]), String([q[1] for q in grid])]
+    # use reverses to make these clockwise
+    [grid[1], column(grid, true), reverse(grid[end]), reverse(column(grid, false))]
 end
 
 function withreverse(lines)
@@ -42,10 +47,17 @@ println("part 1 = ", p1)  # 28057939502729
 
 nblk = isqrt(length(gs))
 npt = length(first(keys(tab)))
-arr = Matrix{Bool}(undef, nblk * npt, nblk * npt);
+arr = Matrix{Char}(undef, nblk * npt, nblk * npt);
 # using count, get one of the four corner ones, and put it in upper left corner
 # using tab, fill the upper row
 # keep filling more rows until whole thing is filled
 # take away the pizza crusts, create another smaller arr
 # create pair stencil for sea monster, try string matching all 8 directions
 # one of them will have the most. Hopefully other 7 have zero.
+
+upperleft = first(k for (k, c) in count if c == 4)
+[length(tab[x]) for x in edges(gs[upperleft])]  # one for outer-facing
+
+# find first 2: that one is the left edge and next one is upper edge
+# the second 2 can be wrapped around
+# stamp that down.
