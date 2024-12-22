f795ea6af1a5f47d647f112ef5813b4401ef672a
David Liu <xdavidliu@gmail.com>
2024-12-22 13:39:02 -0500

    Update day22-part1.jl

--- 2020/day22-part1.jl
+++ 2020/day22-part1.jl
@@ -1,33 +1,11 @@
-# https://docs.julialang.org/en/v1/manual/types/#man-parametric-composite-types
-# below based on 2024 swift util impl, based on CLRS
-
-# surprised 2022 didn't have any BFS until day 22. Today's not even BFS either, just needs queue.
-struct Queue{T}
-    front::Vector{T}
-    back::Vector{T}
-end
-
-import Base.isempty
-import Base.push!
-import Base.pop!
-
-function isempty(q::Queue{T}) where {T}
-    isempty(q.front) && isempty(q.back)
-end
-
-function push!(q::Queue{T}, x::T) where {T}
-    push!(q.back, x)
-end
-
-function pop!(q::Queue{T}) where {T}
-    if isempty(q.front)
-        while length(q.back) > 1
-            push!(q.front, pop!(q.back))
-        end
-        pop!(q.back)
-    else
-        pop!(q.front)
+function appendfronttwo!(a, x1, x2)
+    push!(a, 0)
+    push!(a, 0)
+    for i in reverse(3:length(a))
+        a[i] = a[i-2]
     end
+    a[1] = x1
+    a[2] = x2
 end
 
 function blocknums(bl)
@@ -38,34 +16,25 @@ end
 function playonce!(q1, q2)
     x1, x2 = pop!(q1), pop!(q2)
     if x1 > x2
-        push!(q1, x1)
-        push!(q1, x2)
+        appendfronttwo!(q1, x2, x1)
     elseif x1 < x2
-        push!(q2, x2)
-        push!(q2, x1)
+        appendfronttwo!(q2, x1, x2)
     else
         error("playonce")
     end
 end
 
 function score!(q)
-    a = Vector{Int}()
-    while !isempty(q)
-        push!(a, pop!(q))
-    end
-    reverse!(a)
-    sum(i*x for (i, x) in enumerate(a))
+    sum(i*x for (i, x) in enumerate(q))
 end
 
 function solve()
     filename = "/home/xdavidliu/Documents/input22.txt"
     text = read(filename, String)
     blocks = split(text, "\n\n")
-    a1, a2 = blocknums(rstrip(blocks[1])), blocknums(rstrip(blocks[2]))
-    reverse!(a1)
-    reverse!(a2)
-    q1 = Queue(a1, Vector{Int}())
-    q2 = Queue(a2, Vector{Int}())
+    q1, q2 = blocknums(rstrip(blocks[1])), blocknums(rstrip(blocks[2]))
+    reverse!(q1)
+    reverse!(q2)
     i = 0
     while !isempty(q1) && !isempty(q2)
         playonce!(q1, q2)
