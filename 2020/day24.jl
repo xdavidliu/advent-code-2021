52d38302aaa7872fad0e27cea4d0b17549ffd64c
David Liu <xdavidliu@gmail.com>
2024-12-22 20:54:12 -0500

    Update and rename day24-part1.jl to day24.jl

--- 2020/day24.jl
+++ 2020/day24.jl
@@ -21,6 +21,34 @@ function stepall(text)
     x, y
 end
 
+neighbors(r) = [(r[1]+dx, r[2]+dy) for (dx, dy) in [(-2, 0), (2, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]]
+
+function iter!(black)
+    nbofwhite = Dict{Tuple{Int, Int}, Int}()
+    b2w = Set{Tuple{Int, Int}}()
+    for r0 in black
+        blknb = 0
+        for r in neighbors(r0)
+            if r in black
+                blknb += 1
+            else
+                nbofwhite[r] = get(nbofwhite, r, 0) + 1
+            end
+        end
+        if blknb == 0 || blknb > 2
+            push!(b2w, r0)
+        end
+    end
+    for r in b2w
+        delete!(black, r)
+    end
+    for (r, c) in nbofwhite
+        if c == 2
+            push!(black, r)
+        end
+    end
+end
+
 function solve()
     filename = "/home/xdavidliu/Documents/input24.txt"
     lines = readlines(filename)
@@ -34,6 +62,10 @@ function solve()
         end
     end
     println("part 1 = ", length(black))  # 455
+    for _ in 1:100
+        iter!(black)
+    end
+    println("part 2 = ", length(black))  # 3904
 end
 
 solve()
